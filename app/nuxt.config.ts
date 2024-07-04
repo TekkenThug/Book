d6afe7c1fb8cbc359c6d4ab446f7c4b07e668305
Vadim Ignatov <ignatov0131@gmail.com>
2024-07-04 13:53:57 +0300

    chore: remove unused font

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         { rel: "preconnect", href: "https://fonts.gstatic.com" },
-        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Viga&display=swap" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" },
         { rel: "manifest", href: "/site.webmanifest" }
       ]
     }
