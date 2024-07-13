e432c8000b2538e2df12796d2d6b8aae746eef9b
Vadim Ignatov <ignatov0131@gmail.com>
2024-07-13 17:29:14 +0300

    feat: add swagger for books module

--- books-api/src/books/books.controller.ts
+++ books-api/src/books/books.controller.ts
@@ -1,12 +1,30 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiExtraModels,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { BookDto } from './books.dto';
 
 @ApiTags('Books')
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
 
+  @ApiOperation({ summary: 'Get books' })
+  @ApiQuery({ name: 'title', required: false })
+  @ApiExtraModels(BookDto)
+  @ApiOkResponse({
+    description: 'OK',
+    schema: {
+      type: 'array',
+      items: { $ref: getSchemaPath(BookDto) },
+    },
+  })
   @Get()
   async findAll(@Query() query: { title: string }) {
     return await this.booksService.getFromAPI(query.title);
