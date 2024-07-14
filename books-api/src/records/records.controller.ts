bac986cbf604fb76813583f8a98a3f61a332f8bf
Vadim Ignatov <ignatov0131@gmail.com>
2024-07-14 13:40:11 +0300

    feat: add swagger for records module

--- books-api/src/records/records.controller.ts
+++ books-api/src/records/records.controller.ts
@@ -2,13 +2,44 @@ import { Body, Controller, Post, Req } from '@nestjs/common';
 import { RecordsService } from './records.service';
 import { Request } from 'express';
 import { CreateRecordDto } from './records.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiErrorDto, ApiMessageDto } from '../data/dto';
 
 @ApiTags('Records')
 @Controller('records')
 export class RecordsController {
   constructor(private recordsService: RecordsService) {}
 
+  @ApiOperation({ summary: 'Create record to event' })
+  @ApiCreatedResponse({
+    description: 'Record created',
+    type: ApiMessageDto,
+    example: { message: 'You successfully recorded' },
+  })
+  @ApiBadRequestResponse({
+    description: 'Record already exists',
+    type: ApiErrorDto,
+    example: {
+      statusCode: 400,
+      message: 'Record already exists',
+      error: 'Bad request',
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Event not found',
+    type: ApiErrorDto,
+    example: {
+      statusCode: 404,
+      message: 'Event not found',
+      error: 'Not found',
+    },
+  })
   @Post()
   async create(
     @Body() createRecordDto: CreateRecordDto,
