87c155cfcc5b43cf3c5e97349c93c0f40e082ff3
Vadim Ignatov <ignatov0131@gmail.com>
2024-07-17 10:55:36 +0300

    fix: eslint fix

--- api/src/config/morgan.config.ts
+++ api/src/config/morgan.config.ts
@@ -1,5 +1,5 @@
 import morgan from 'morgan';
 
 export default morgan('[HTTP] :method :url :status - :response-time ms', {
-  skip: (req, res) => req.method === 'OPTIONS',
+  skip: (req) => req.method === 'OPTIONS',
 });
