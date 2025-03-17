b07c6b6e54e7ea3a6c578f027f2a113a3371e346
Vadim Ignatov <ignatov0131@gmail.com>
2025-03-17 11:53:34 +0300

    fix: add middleware for auth

--- app/services/api/client.ts
+++ app/services/api/client.ts
@@ -1,8 +1,22 @@
-import createClient from "openapi-fetch";
+import createClient, { type Middleware } from "openapi-fetch";
+import { useLocalStorage } from "@vueuse/core";
 import type { paths } from "./v1";
 
 export type Paths = paths;
 
-const client = createClient<paths>({ baseUrl: "http://localhost:8000/api/v1" });
+const accessToken = useLocalStorage<string | null>("token", null);
+
+const authMiddleware: Middleware = {
+	async onRequest({ request }) {
+		if (accessToken.value) {
+			request.headers.set("Authorization", `Bearer ${accessToken.value}`);
+		}
+
+		return request;
+	},
+};
+
+const client = createClient<paths>({ baseUrl: "http://localhost:8000/" });
+client.use(authMiddleware);
 
 export default client;
